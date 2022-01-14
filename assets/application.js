4c8826899fbf5b985de1dd29d981b250c3ee17c3
Vitaliy <nertis44@gmail.com>
2022-01-14 17:49:43 +0200

    sdcsd

--- assets/application.js
+++ assets/application.js
@@ -1,6 +1,7 @@
 // Put your application javascript here
 // alert(1)
 
+if(document.querySelector('#sort_by')!=null)
 document.querySelector('#sort_by').addEventListener('change', function (e){
     let url = new URL(window.location.href);
     url.searchParams.set('sort_by', e.currentTarget.value)
