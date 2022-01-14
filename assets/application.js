94143dbd55a32a7a08d3cb7123f026f0f523217f
Vitaliy <nertis44@gmail.com>
2022-01-14 15:02:00 +0200

    sdcsdcsd

--- assets/application.js
+++ assets/application.js
@@ -1,2 +1,8 @@
 // Put your application javascript here
 // alert(1)
+
+document.querySelector('#sort_by').addEventListener('change', function (e){
+    let url = new URL(window.location.href);
+    url.searchParams.set('sort_by', e.currentTarget.value)
+    window.location = url.href
+})
